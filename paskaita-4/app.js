2cf25e258349f300a6ecadcdd3e084c72e81ee12
Martynas Druteika <martynas.druteika@devbridge.com>
2022-12-05 18:05:12 +0200

    paskaita 4

--- paskaita-4/app.js
+++ paskaita-4/app.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-const TASKS = [
+let TASKS = [
     { id: 1, title: 'Išplauti indus', isDone: false },
     { id: 2, title: 'Išsiurbti namus', isDone: true },
     { id: 3, title: 'Pasimokyti programuoti', isDone: false },
@@ -21,6 +21,32 @@ app.get('/api/tasks/:id', (req, res) => {
     const id = Number(req.params.id);
     const task = TASKS.find((task) => task.id === id);
     res.send(task);
+});
+
+app.post('/api/tasks', (req, res) => {
+    const lastTask = TASKS[TASKS.length - 1];
+    const newTask = { ...req.body, id: lastTask.id + 1, isDone: false };
+
+    TASKS.push(newTask);
+    res.send(TASKS);
+});
+
+app.put('/api/tasks/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const newTask = req.body;
+
+    const updatedTasks = TASKS.map((task) => {
+        if (task.id === id) {
+            return {
+                ...newTask,
+                id: task.id
+            };
+        }
+        return task;
+    });
+
+    TASKS = [...updatedTasks];
+    res.send(TASKS);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ paskaita-4/main.js
@@ -0,0 +1,63 @@
+const tasksContainer = document.getElementById('tasks');
+const addTaskForm = document.getElementById('addTaskForm');
+const taskTitleInput = document.getElementById('taskTitleInput');
+
+const BASE_ENDPOINT = 'http://localhost:3000';
+const TASKS_ENDPOINT = BASE_ENDPOINT + '/api/tasks';
+const GET_FIRST_TASK_ENDPOINT = TASKS_ENDPOINT + '/1';
+
+addTaskForm.addEventListener('submit', (event) => {
+    // event.preventDefault();
+    fetch(TASKS_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title: taskTitleInput.value })
+    });
+});
+
+fetch(GET_FIRST_TASK_ENDPOINT)
+    .then((res) => res.json())
+    .then((task) => {
+        const firstTask = document.createElement('h1');
+        firstTask.textContent = `Pirmas taskas eilėje yra ${task.title}`
+        tasksContainer.appendChild(firstTask);
+    });
+
+fetch(TASKS_ENDPOINT)
+    .then((res) => res.json())
+    .then((tasks) => {
+        tasks.forEach((task) => {
+            const card = document.createElement('div');
+            card.style.display = 'flex';
+            card.style.alignItems = 'center';
+
+            const title = document.createElement('h2');
+            title.textContent = task.title;
+
+            title.style.color = task.isDone ? 'green' : 'red';
+
+            const completeButton = document.createElement('button');
+            completeButton.textContent = 'Complete';
+
+            completeButton.addEventListener('click', () => {
+                fetch(`http://localhost:3000/api/tasks/${task.id}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ ...task, isDone: true })
+                }).then((res) => res.json())
+                .then((tasks) => {
+                    window.location.reload();
+                });
+            });
+
+            card.appendChild(title);
+            card.appendChild(completeButton);
+
+            tasksContainer.appendChild(card);
+        });
+    });
+
