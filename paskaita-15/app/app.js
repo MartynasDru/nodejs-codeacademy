56bd5577f9cd5b73d22ab6a289830bd1ffc9730c
Martynas Druteika <martynas.druteika@devbridge.com>
2022-12-21 20:13:39 +0200

    paskaita 15

---
+++ paskaita-15/app/app.js
@@ -0,0 +1,79 @@
+const cors = require('cors');
+const mysql = require('mysql2');
+
+const express = require('express');
+const { connect } = require('mongoose');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const mysqlConfig = {
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'test123',
+  database: 'Shop',
+  port: 3306
+};
+
+const connection = mysql.createConnection(mysqlConfig);
+
+//ENDPOINTS
+app.get('/products', (req, res) => {
+  connection.execute('SELECT * FROM Products', (err, result) => {
+    res.send(result);
+  });
+});
+
+app.post('/employees', (req, res) => {
+  connection.execute('INSERT INTO Employees (name, salary) VALUES (?, ?)', [req.body.name, req.body.salary], (err, result) => {
+    console.log(err);
+    connection.execute('SELECT * FROM Employees', (err, result) => {
+      res.send(result);
+    });
+  });
+});
+
+app.post('/products', (req, res) => {
+  const { title, price, stock } = req.body;
+
+  connection.execute(
+    'INSERT INTO Products (title, price, stock) VALUES (?, ?, ?)', 
+    [title, price, stock],
+    (err, result) => {
+      connection.execute('SELECT * FROM Products', (err, result) => {
+        res.send(result);
+      });
+    }
+  )
+});
+
+app.patch('/employees/:id', (req, res) => {
+  const { name, salary } = req.body;
+  connection.execute(
+    'UPDATE Employees SET name=?, salary=? WHERE id=?', 
+    [name, salary, req.params.id],
+    (err, result) => {
+      connection.execute('SELECT * FROM Employees', (err, result) => {
+        res.send(result);
+      })
+    }
+  )
+});
+
+app.patch('/products/:id', (req, res) => {
+  const { title, price, stock } = req.body;
+  connection.execute(
+    'UPDATE Products SET title=?, price=?, stock=? WHERE id=?', 
+    [title, price, stock, req.params.id],
+    (err, result) => {
+      connection.execute('SELECT * FROM Products', (err, result) => {
+        res.send(result);
+      })
+    }
+  )
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
---
+++ paskaita-15/client/main.js
@@ -0,0 +1,123 @@
+const form = document.getElementById('form');
+const editForm = document.getElementById('edit-form');
+const brandInput = document.getElementById('brand');
+const modelInput = document.getElementById('model');
+const priceInput = document.getElementById('price');
+const advertsOutput = document.getElementById('adverts');
+const editFormOutput = document.getElementById('edit-form-output');
+const editBrandInput = document.getElementById('edit-brand-input');
+const editModelInput = document.getElementById('edit-model-input');
+const editPriceInput = document.getElementById('edit-price-input');
+const cancelEditFormButton = document.getElementById('cancel-edit-button');
+const descriptionInput = document.getElementById('description');
+
+const USER_ID = '639760429f7ee04711be213c';
+const BASE_URL = 'http://localhost:3000';
+
+let editAdvertId;
+
+editForm.classList.add('hidden');
+
+editForm.addEventListener('submit', () => {
+    const updatedAdvert = {
+        brand: editBrandInput.value,
+        model: editModelInput.value,
+        price: editPriceInput.value
+    };
+
+    fetch(BASE_URL + '/adverts/' + editAdvertId, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedAdvert)
+    });
+});
+
+form.addEventListener('submit', (event) => {
+    const brand = brandInput.value;
+    const model = modelInput.value;
+    const price = priceInput.value;
+    const description = descriptionInput.value;
+
+    const newAdvert = {
+        brand,
+        model,
+        price,
+        user_id: USER_ID,
+        description
+    };
+
+    fetch(BASE_URL + '/adverts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newAdvert)
+    });
+});
+
+cancelEditFormButton.addEventListener('click', (req, res) => {
+    editForm.classList.add('hidden');
+    editBrandInput.value = '';
+    editModelInput.value = '';
+    editPriceInput.value = '';
+});
+
+fetch(BASE_URL + '/adverts')
+    .then((res) => res.json())
+    .then((adverts) => {
+        adverts.forEach((advert) => createAdvertCard(advert));
+    });
+
+function createAdvertCard(advert) {
+    const advertCard = document.createElement('div');
+    advertCard.classList.add('advert-card');
+    
+    const advertBrand = document.createElement('h3');
+    advertBrand.textContent = advert.brand;
+
+    const advertModel = document.createElement('p');
+    advertModel.textContent = advert.model;
+
+    const advertPrice = document.createElement('p');
+    advertPrice.textContent = advert.price;
+
+    const advertDescription = document.createElement('p');
+    advertDescription.textContent = advert.description;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'DELETE';
+    deleteButton.classList.add('delete-button');
+
+    deleteButton.addEventListener('click', () => {
+        fetch(BASE_URL + '/adverts/' + advert._id, {
+            method: 'DELETE'
+        })
+        .then(() => window.location.reload());
+    });
+
+    const editButton = document.createElement('button');
+    editButton.textContent = 'EDIT';
+    editButton.classList.add('edit-button');
+    editButton.style.marginLeft = '10px';
+
+    editButton.addEventListener('click', () => {
+        editForm.classList.remove('hidden');
+        
+        editBrandInput.value = advert.brand;
+        editModelInput.value = advert.model;
+        editPriceInput.value = advert.price;
+
+        editAdvertId = advert._id;
+    });
+
+    advertCard.appendChild(advertBrand);
+    advertCard.appendChild(advertModel);
+    advertCard.appendChild(advertPrice);
+    advertCard.appendChild(advertDescription);
+    advertCard.appendChild(deleteButton);
+    advertCard.appendChild(editButton);
+
+    advertsOutput.appendChild(advertCard);
+}
